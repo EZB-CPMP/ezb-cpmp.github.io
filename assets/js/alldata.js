4f60a118be6f2f554379fafdc55a0b1007e5b40a
Administrator <gitlab_admin_e10954@example.com>
2024-10-19 10:28:36 +0800

    sdfasdf

--- assets/js/alldata.js
+++ assets/js/alldata.js
@@ -13,6 +13,11 @@ var XFCAPP = {
     'APP都安全无毒，狼友们多装几款备用！',
     '性福草官方APP',
   ],
+  siteList:[
+    {url:"https://xnxx.com",name:"免费XNXX在线",star:"1500",sort:1,desc:"免费观看部分地区打开异常",typeText:"进入",image:""},
+    {url:"https://xvideos.com",name:"免费XVIDEOS",star:"1500",sort:1,desc:"免费观看部分地区打开异常",typeText:"进入",image:""},
+    {url:"https://xnxx.com",name:"好色TV",star:"1500",sort:1,desc:"免费观看部分地区打开异常",typeText:"进入",image:""},
+  ],
   videoList:[
     'http://www.bjees.com/',
     'http://xnxx.xfc01.com/',
