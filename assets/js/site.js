38a0c35da6459bdcb868832ed3a6f3eebf3679b9
Administrator <gitlab_admin_e10954@example.com>
2024-10-19 10:23:51 +0800

    sdfasdf

---
+++ assets/js/site.js
@@ -0,0 +1,5 @@
+SITEList:[
+    {url:"https://xnxx.com",name:"免费XNXX在线",star:"1500",sort:1,desc:"免费观看部分地区打开异常",typeText:"进入",image:""},
+    {url:"https://xvideos.com",name:"免费XVIDEOS",star:"1500",sort:1,desc:"免费观看部分地区打开异常",typeText:"进入",image:""},
+    {url:"https://xnxx.com",name:"好色TV",star:"1500",sort:1,desc:"免费观看部分地区打开异常",typeText:"进入",image:""},
+]
