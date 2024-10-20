2cf0d8ad65505b54343181fb05ef42d5b199316b
Administrator <gitlab_admin_e10954@example.com>
2024-10-20 20:39:36 +0800

    sdfasdf

--- assets/js/alldata.js
+++ assets/js/alldata.js
@@ -1,5 +1,6 @@
-XFCAPP = {
+var XFCAPP = {
   version:"1.0.1",
+  common:"https://www.sebocao.cc",
   shareURL: "https://download.xfc01.com/d/xfc.apk",
   appDowloadURL:"https://download.xfc01.com/d/xfc.apk",
   account: "@xfcapp",
@@ -11,18 +12,19 @@ XFCAPP = {
   topNotice:[
     '商务联系 @xfcapp',
     'APP都安全无毒，狼友们多装几款备用！',
-    '性福草官方APP',
+    '性福宝官方APP',
   ],
   siteList:[
-    {url:"https://xnxx.com",name:"免费XNXX在线",star:"1500",viewNum:0,sort:1,desc:"免费观看部分地区打开异常",typeText:"进入",image:""},
-    {url:"https://xvideos.com",name:"免费XVIDEOS",star:"1500",sort:1,viewNum:0,desc:"免费观看部分地区打开异常",typeText:"进入",image:""},
-    {url:"https://xnxx.com",name:"好色TV",star:"1500",sort:1,viewNum:0,desc:"免费观看部分地区打开异常",typeText:"进入",image:""},
+    {url:"https://xnxx.zhikangapp.com",name:"免费XNXX在线",star:"1500",viewNum:0,sort:1,desc:"免费观看部分地区打开异常",typeText:"进入",image:""},
+    {url:"https://xvideos.xxx-xnxx-xvideos.com",name:"免费XVIDEOS",star:"1500",sort:1,viewNum:0,desc:"免费观看部分地区打开异常",typeText:"进入",image:""},
+    {url:"https://hs.xingfucao.xyz",name:"好色TV",star:"1500",sort:1,viewNum:0,desc:"免费观看部分地区打开异常",typeText:"进入",image:""},
   ],
   videoList:[
-    'http://www.bjees.com/',
-    'http://xnxx.xfc01.com/',
-    'http://xvideos.xfc01.com/'
+    'http://awww.xnxxsp.com',
+    'http://xnxx.mnxnxx.com',
+    'http://xxx.xinzhipayment.com'
   ],
+  iframevideolist:[],
   fabuDOM:`
         <div>
             <div style="text-align:center;"><img src="./assets/images/common/logo.png" width="60" height="60"></div>
@@ -38,11 +40,12 @@ XFCAPP = {
   notice:"",
   gonggaoDom: `
         <div>
+            <div style="text-align:center;"><img src="./assets/images/common/logo.png" width="60" height="60"></div>
             <h1 style="color:red;padding:5px 0;">视频免费视频在线看！</h1>
         </div>
       `,
   gonggaoAPPdom: `
           <div>
           </div>
-         `
+        `
 }
