f2267b4829bd1ffaca3bcad826399e98de857d96
Administrator <gitlab_admin_e10954@example.com>
2024-10-25 12:02:23 +0800

    fasdf

--- assets/js/alldata.js
+++ assets/js/alldata.js
@@ -12,17 +12,15 @@ var XFCAPP = {
   topNotice:[
     '商务联系 @xfcapp',
     'APP都安全无毒，狼友们多装几款备用！',
-    '性福宝官方APP',
+    '性福宝官方APP.',
   ],
   siteList:[
     {url:"http://xnxx.zhikangapp.com",name:"免费XNXX",star:"7546414",viewNum:0,sort:1,desc:"免费观看部分地区打开异常",typeText:"进入",image:""},
     {url:"http://xvideos.xxx-xnxx-xvideos.com",name:"免费XVIDEOS",star:"5314412",sort:1,viewNum:0,desc:"免费观看部分地区打开异常",typeText:"进入",image:""},
-    {url:"http://hs.sebocao.xyz",name:"好色TV",star:"23441",sort:1,viewNum:0,desc:"免费观看部分地区打开异常",typeText:"进入",image:""},
+    {url:"http://hs.sebocao.xyz",name:"好色TV",star:"234410",sort:1,viewNum:0,desc:"免费观看部分地区打开异常",typeText:"进入",image:""},
   ],
   videoList:[
-    'http://awww.xnxxsp.com',
-    'http://xnxx.mnxnxx.com',
-    'http://xxx.xinzhipayment.com'
+    'http://hs.sebocao.xyz',
   ],
   iframevideolist:[],
   fabuDOM:`
